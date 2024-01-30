5f1f0801659a528aaa79a76c7348d5889fbade86
QCTL <qctl@outlook.es>
2024-01-30 13:45:15 +0100

    Completed B16, B6

---
+++ src/display/layers/tileset/gTileset.h
@@ -0,0 +1,36 @@
+//
+// Created by Laminar on 30/01/2024.
+//
+
+#ifndef CITYOFWEIRDFISHES_GTILESET_H
+#define CITYOFWEIRDFISHES_GTILESET_H
+
+#include <string>
+#include <SFML/Graphics/Texture.hpp>
+
+class gTileset{
+public:
+    gTileset(std::string pthTS, int nPixelsSize, int tWidth, int tHeight):
+        tsNWidth(tWidth), tsNHeight(tHeight), tsSize(nPixelsSize){
+        if (!tsTex.loadFromFile(R"(C:\Users\Robert\CityOfWeirdFishes\files\graphic\tilesets\)" + pthTS)) {
+            std::cout << "WARNING: A TEXTURE HAS NOT LOADED PROPERLY" << std::endl;
+        }
+    }
+
+    std::pair<int,int> getPos(int i){
+        return {i%tsNWidth, i/tsNWidth};
+    }
+
+    int getTileSize() const{
+        return tsSize;
+    }
+    sf::Texture tsTex;
+
+private:
+
+    int tsNWidth;
+    int tsNHeight;
+    int tsSize;
+};
+
+#endif //CITYOFWEIRDFISHES_GTILESET_H
