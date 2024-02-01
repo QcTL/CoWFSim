fd452696104541da52427fbe2ed47e7351d52b9e
QCTL <qctl@outlook.es>
2024-02-01 12:43:55 +0100

    Completed b18 Generic Kernel transformation

--- src/sim/structure/grids/transformation/gBasicTransformations.h
+++ src/sim/structure/grids/transformation/gBasicTransformations.h
@@ -13,40 +13,62 @@ class BasicTransformations {
 
 public:
 
-    template<typename T>
-    static void GaussianFilter(std::shared_ptr<gIGrid<T>> grid) {
-        std::vector<T> nAttributes;
-        std::pair<std::pair<int, int>, std::pair<int, int>> range = grid->rangeUse();
 
-        static std::vector<T> dValues = {0.125, 0.125, 0.125, 0.125, 0.0625, 0.0625, 0.0625, 0.0625};
-        for (int i = range.second.first; i < (range.second.second + 1); i++) {
-            for (int j = range.first.first; j < (range.first.second + 1); j++) {
-                std::vector<std::pair<int, int>> dOffsets =
-                        {{i + 1, j},
-                         {i - 1, j},
-                         {i,     j + 1},
-                         {i,     j - 1},
-                         {i + 1, j + 1},
-                         {i - 1, j + 1},
-                         {i + 1, j - 1},
-                         {i - 1, j - 1}};
-                T r = 0.25 * grid->get(i, j);
-                for (int k = 0; k < dOffsets.size(); k++) {
-                    if (grid->isInside(dOffsets[k].first, dOffsets[k].second)) {
-                        r += dValues[k] * grid->get(dOffsets[k].first, dOffsets[k].second);
-                    }
-                }
-                nAttributes.push_back(r);
+    template<typename T>
+    struct Kernel {
+        std::vector<T> dValues;
+        int sX;
+        int sY;
+
+        Kernel(const std::vector<T> &dValues, int sX, int sY) : dValues(dValues), sX(sX), sY(sY) {}
+    };
+
+    template<typename T>
+    static void GenericKernelTransformation(std::shared_ptr<gIGrid<T>> grid, Kernel<T> kernel) {
+        auto range = grid->rangeUse();
+        int width = range.first.second - range.first.first + 1;
+        std::vector<T> nAttributes(width*(range.second.second - range.second.first + 1), 0);
+
+        std::vector<std::pair<int, int>> kernelIndices;
+        std::vector<T> kernelValues;
+        for(int y = 0; y < kernel.sY; y++){
+            for(int x = 0; x < kernel.sX; x++){
+                kernelIndices.push_back({x - kernel.sX/2, y - kernel.sY/2});
+                kernelValues.push_back(kernel.dValues[y * kernel.sX + x]);
             }
         }
 
-        int width = range.first.second - range.first.first + 1;
-
-        for (int i = 0; i < nAttributes.size(); i++) {
-            int x = range.first.first + i % width;
-            int y = range.second.first + i / width;
-            grid->set(y, x, nAttributes[i]);
+        int idx = 0;
+        for (int i = range.second.first; i <= range.second.second; i++) {
+            for (int j = range.first.first; j <= range.first.second; j++, idx++) {
+                T r = 0;
+                for(int k = 0; k < kernelIndices.size(); k++){
+                    int dx = i + kernelIndices[k].first;
+                    int dy = j + kernelIndices[k].second;
+                    if (grid->isInside(dx, dy)) {
+                        r += kernelValues[k] * grid->get(dx, dy);
+                    }
+                }
+                nAttributes[idx] = r;
+            }
         }
+
+        idx = 0;
+        for (int i = range.second.first; i <= range.second.second; i++) {
+            for (int j = range.first.first; j <= range.first.second; j++, idx++) {
+                grid->set(i, j, nAttributes[idx]);
+            }
+        }
+    }
+
+
+
+
+    template<typename T>
+    static void GaussianFilter(std::shared_ptr<gIGrid<T>> grid) {
+
+        BasicTransformations::Kernel<double> k({0.0625,0.125, 0.0625,0.125,0.25,0.125,0.0625,0.125, 0.0625},3,3);
+        BasicTransformations::GenericKernelTransformation<T>(grid, k);
     }
 
 };
